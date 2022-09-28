681837072e3ceee928bed81088db54aa8a23dc0e
AGuerrero123 <78996007+AGuerrero123@users.noreply.github.com>
2022-09-28 14:55:26 -0500

    Changed design of original WeatherApp
    added geolocation api and weather api

--- src/index.js
+++ src/index.js
@@ -23,13 +23,34 @@ function formatDate(date) {
     return `${day} ${hours}:${minutes}`;
   }
 
-function search(event){
+function displayWeatherConditions(response){
+console.log(response);
+document.querySelector("#city").innerHTML = response.data.name;
+document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+document.querySelector("#description").innerHTML = response.data.weather[0].main;
+}
+function handleSubmit(event){
     event.preventDefault();
-    let cityElement = document.querySelector("#city");
-    let cityInput = document.querySelector("#city-input");
-    cityElement.innerHTML = cityInput.value;
+    let city = document.querySelector("#city-input").value;
+    searchCity(city);
 } 
-
+function searchCity(city){
+  let apiKey = `be60748992fab0f5da8162563fb21245`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&&units=metric&units=metric`;
+axios.get(apiUrl).then(displayWeatherConditions)
+}
+function searchLocation(position){
+  let apiKey = `be60748992fab0f5da8162563fb21245`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayWeatherConditions)
+}
+function getCurrentLocation(event){
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
 function convertToFahrenheit(event){
     event.preventDefault();
     let temperatureElement = document.querySelector("#temperature");
@@ -48,7 +69,7 @@ dateElement.innerHTML = formatDate(currentTime);
 
 //Feature Two
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+searchForm.addEventListener("submit", handleSubmit);
 
 //Feature Three
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
@@ -56,3 +77,8 @@ fahrenheitLink.addEventListener("click", convertToFahrenheit)
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
+
+//Feature Four
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+searchCity("Tokyo");

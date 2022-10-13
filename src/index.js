78540a4d0cd35556170ed15664a27190df9fdb25
AGuerrero123 <78996007+AGuerrero123@users.noreply.github.com>
2022-10-13 16:38:26 -0500

    Implementation of weather api

--- src/index.js
+++ src/index.js
@@ -22,15 +22,15 @@ function formatDate(date) {
 
   return `${day} ${hours}:${minutes}`;
 }
-function getforecast(coordinates) {
+function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = `be60748992fab0f5da8162563fb21245`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 function displayForecast(response) {
-  console.log(response.data);
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Thu", "Fri", "Sat"];
@@ -73,7 +73,7 @@ function displayWeatherConditions(response) {
   document
     .querySelector("#icon")
     .setAttribute("alt", response.data.weather[0].main);
-  getforecast(response.data.coord);
+  getForecast(response.data.coord);
 }
 function handleSubmit(event) {
   event.preventDefault();

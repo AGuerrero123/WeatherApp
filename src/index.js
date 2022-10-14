279224b9f912e73349ca3b436a1e0275ab3887d9
AGuerrero123 <78996007+AGuerrero123@users.noreply.github.com>
2022-10-14 02:03:24 -0500

    updated api changes to display 6 days

--- src/index.js
+++ src/index.js
@@ -22,6 +22,13 @@ function formatDate(date) {
 
   return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = `be60748992fab0f5da8162563fb21245`;
@@ -30,22 +37,30 @@ function getForecast(coordinates) {
   axios.get(apiUrl).then(displayForecast);
 }
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
   <div class="col-2">
-    <div class="weather-forecast-date">${day}</div>
-    <img src="http://openweathermap.org/img/wn/10n@2x.png" alt=""width="42">
+    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+    <img src="http://openweathermap.org/img/wn/${
+      forecastDay.weather[0].icon
+    }@2x.png" alt=""width="42">
     <div class="weather-forecast-temp">
-      <span class="weather-forecast-temp-max">18°</span>
-      <span class="weather-forecast-temp-min">12°</span>
+      <span class="weather-forecast-temp-max">${Math.round(
+        forecastDay.temp.max
+      )}°</span>
+      <span class="weather-forecast-temp-min">${Math.round(
+        forecastDay.temp.min
+      )}°</span>
     </div>
   </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
